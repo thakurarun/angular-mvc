65732727a9ea16bbda0544a1dd4a58c5add87f26
thakurarun2 <arun_thakur@live.com>
2015-12-22 18:34:19 +0530

    custom validation added....

--- com/Scripts/application/all.js
+++ com/Scripts/application/all.js
@@ -76,6 +76,7 @@ app.directive('textArea', function () {
     }
     return {
         restrict: 'E',
+        replace: true,
         scope: {
             max: '@',
             min: '@',
@@ -87,6 +88,62 @@ app.directive('textArea', function () {
     };
 });
 
+app.directive('textBox', function () {
+    var minMessage = '{0} more to go...';
+    var maxMessage = '{0} characters left.';
+    var defaultMessage = 'enter at least {0} characters';;
+    function link(scope, element, attrs) {
+        scope.message = defaultMessage.format(scope.min);
+        element.on('keyup',  function (e) {
+            var len = $.trim(e.target.value).length;
+            var msg = '';
+            var left = 0;
+            if (len == 0) {
+                msg = defaultMessage.format(scope.min);
+            } else if (len >= scope.min) {
+                left = scope.max - len;
+                msg = maxMessage.format(left);;
+            } else if (len < scope.min) {
+                left = scope.min - len;
+                msg = minMessage.format(left);
+            }
+            scope.$apply(function () {
+                scope.message = msg;
+            });
+        });
+    }
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            max: '@',
+            min: '@',
+            message: '@'
+        },
+        templateUrl: RootClientUrl + 'CustomControls/TextBoxControl/templateTextBox.html',
+        link: link
+    };
+});
+
+app.directive("compareTo", function () {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function (scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function (modelValue) {
+                //debugger; 
+                return modelValue == scope.otherModelValue.$viewValue;
+            };
+
+            scope.$watch("otherModelValue", function () {
+                ngModel.$validate();
+            });
+        }
+    };
+});
 // First, checks if it isn't implemented yet.
 if (!String.prototype.format) {
     String.prototype.format = function () {
